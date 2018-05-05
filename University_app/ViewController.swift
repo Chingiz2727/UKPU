f17100d6f628f32dd1c8377e521f206d7d3f6e6c
Чингиз Куандик <chingiz@macbook-pro-cingiz.local>
2018-05-05 14:50:28 +0600

    Initial Commit

---
+++ University_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  University_app
+//
+//  Created by Чингиз on 05.05.2018.
+//  Copyright © 2018 Чингиз. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
